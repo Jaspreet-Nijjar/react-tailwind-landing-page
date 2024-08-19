426f8d7c5fe374f8acfe77b4cebde3b60697c7f0
Jaspreet <jaspreetkn92@gmail.com>
2024-08-19 09:34:45 +0100

    added sections to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import {
+  Companies,
+  Clipboard,
+  Footer,
+  Header,
+  Hero,
+  Workflow,
+} from './sections/index';
 
 const App = () => {
-  return <div>Project</div>;
+  return (
+    <main className="max-w-md mx-auto flex flex-col items-center min-h-screen border border-red-500 text-center">
+      <Header />
+      <Hero />
+      <Clipboard />
+      <Workflow />
+      <Companies />
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
