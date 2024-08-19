b297903718cfde856200700fe039749e1be73aa9
Jaspreet <jaspreetkn92@gmail.com>
2024-08-19 09:30:11 +0100

    extended colours in the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayBlue: 'hsl(210, 10%, 33%)',
+        grayBlue: 'hsl(201, 11%, 66%)',
+      },
+    },
   },
   plugins: [],
 };
