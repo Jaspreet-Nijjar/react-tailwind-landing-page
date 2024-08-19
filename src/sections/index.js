1e9e74b716414911ba894997e8e36612258b49ce
Jaspreet <jaspreetkn92@gmail.com>
2024-08-19 09:33:35 +0100

    created sections and index to export pages

---
+++ src/sections/index.js
@@ -0,0 +1,8 @@
+import Header from './Header';
+import Hero from './Hero';
+import Clipboard from './Clipboard';
+import Workflow from './Workflow';
+import Footer from './Footer';
+import Companies from './Companies';
+
+export { Header, Hero, Clipboard, Workflow, Footer, Companies };
