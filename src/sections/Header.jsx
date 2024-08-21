f50ad273decb069b9c2aabaf3a8e49e2751533a6
Jaspreet <jaspreetkn92@gmail.com>
2024-08-21 18:58:03 +0100

    added background and logo

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,13 @@
+import logo from '../../images/logo.svg';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="text-center">
+      <div className="bg-[url('../../images/bg-header-mobile.png')] bg-cover">
+        <img src={logo} alt="logo" className="mx-auto p-6" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
