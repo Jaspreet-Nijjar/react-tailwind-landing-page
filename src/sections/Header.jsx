394ba237587189419e1e4cc5f9be324afc82f6e5
Jaspreet <jaspreetkn92@gmail.com>
2024-08-21 18:59:32 +0100

    added text elements

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -6,6 +6,16 @@ const Header = () => {
       <div className="bg-[url('../../images/bg-header-mobile.png')] bg-cover">
         <img src={logo} alt="logo" className="mx-auto p-6" />
       </div>
+
+      <h1 className="text-2xl font-bold p-6 font-jamjuree text-darkGrayBlue">
+        A history of
+        <br />
+        everything you copy
+      </h1>
+      <p className="text-grayBlue mb-8">
+        Clipboard allows you to track and organize everything you copy.
+        Instantly access your clipboard on all your devices.
+      </p>
     </header>
   );
 };
