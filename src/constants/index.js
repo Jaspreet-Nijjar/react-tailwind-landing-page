909c52dd9202a1fabe49205bb216696f84b6e4c7
Jaspreet <jaspreetkn92@gmail.com>
2024-08-25 10:27:03 +0100

    created the clipboard section

---
+++ src/constants/index.js
@@ -0,0 +1,116 @@
+import blacklist from '../../images/icon-blacklist.svg';
+import text from '../../images/icon-text.svg';
+import preview from '../../images/icon-preview.svg';
+
+import google from '../../images/logo-google.png';
+import hp from '../../images/logo-hp.png';
+import ibm from '../../images/logo-ibm.png';
+import microsoft from '../../images/logo-microsoft.png';
+import vector from '../../images/logo-vector-graphics.png';
+
+import twitter from '../../images/icon-twitter.svg';
+import facebook from '../../images/icon-facebook.svg';
+import instagram from '../../images/icon-instagram.svg';
+
+export const hero = [
+  {
+    heading: 'Quick Search',
+    text: 'Easily search your snippets by content, category, web address, application, and more.',
+  },
+  {
+    heading: 'iCloud Sync',
+    text: 'Instantly saves and syncs snippets across all your devices.',
+  },
+  {
+    heading: 'Complete History',
+    text: 'Retrieve any snippets from the first moment you started using the app.',
+  },
+];
+
+export const workflow = [
+  {
+    icon: blacklist,
+    heading: 'Create blacklists',
+    text: 'Ensure sensitive information never makes its way to your clipboard by excluding certain sources',
+  },
+
+  {
+    icon: text,
+    heading: 'Plain text snippets',
+    text: 'Remove unwanted formatting from copied text for a consistent look',
+  },
+
+  {
+    icon: preview,
+    heading: 'Sneak preview',
+    text: 'Quick preview of all snippets on your Clipboard for easy access',
+  },
+];
+
+export const companyImages = [
+  {
+    name: 'Google',
+    image: google,
+  },
+  {
+    name: 'HP',
+    image: hp,
+  },
+
+  {
+    name: 'IBM',
+    image: ibm,
+  },
+
+  {
+    name: 'Microsoft',
+    image: microsoft,
+  },
+
+  {
+    name: 'Vector Graphics',
+    image: vector,
+  },
+];
+
+export const footerLinks = [
+  {
+    linkName: 'FAQs',
+    link: '/',
+  },
+
+  {
+    linkName: 'Contact Us',
+    link: '/',
+  },
+
+  {
+    linkName: 'Privacy Policy',
+    link: '/',
+  },
+
+  {
+    linkName: 'Press Kit',
+    link: '/',
+  },
+
+  {
+    linkName: 'Install Guide',
+    link: '/',
+  },
+];
+
+export const socialMediaIcons = [
+  {
+    name: 'Facebook',
+    image: facebook,
+  },
+  {
+    name: 'Twitter',
+    image: twitter,
+  },
+  {
+    name: 'Instagram',
+    image: instagram,
+  },
+];
--- src/sections/Clipboard.jsx
+++ src/sections/Clipboard.jsx
@@ -1,5 +1,22 @@
+import Button from '../components/Button';
+
 const Clipboard = () => {
-  return <div>Clipboard</div>;
+  return (
+    <section className="mt-12">
+      <h1 className="font-bold text-darkGrayBlue text-2xl">
+        Clipboard for iOS and Mac OS
+      </h1>
+      <p className="text-grayBlue mt-2 mb-6">
+        Available for free on the App Store. Download for Mac or iOS, sync with
+        iCloud and you're ready to start adding to your clipboard
+      </p>
+
+      <div className="md:flex">
+        <Button text="iOS" />
+        <Button text="Mac" />
+      </div>
+    </section>
+  );
 };
 
 export default Clipboard;
