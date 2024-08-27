4580d83a0a26f6dc51613edf2d308966dafa3c7d
Jaspreet <jaspreetkn92@gmail.com>
2024-08-27 16:51:50 +0100

    added the companies section

--- src/sections/Companies.jsx
+++ src/sections/Companies.jsx
@@ -1,5 +1,17 @@
+import { companyImages } from '../constants';
+
 const Companies = () => {
-  return <div>Companies</div>;
+  return (
+    <div className="flex flex-col gap-12 lg:flex-row lg:gap-16 px-8">
+      {companyImages.map((companyImage) => (
+        <img
+          src={companyImage.image}
+          alt={companyImage.name}
+          key={companyImage.name}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Companies;
