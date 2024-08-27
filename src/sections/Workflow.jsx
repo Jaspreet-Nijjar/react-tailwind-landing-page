2c2d3779f36cae62add1fa9501df58f96201fd93
Jaspreet <jaspreetkn92@gmail.com>
2024-08-27 16:51:19 +0100

    added the workflow section

--- src/sections/Workflow.jsx
+++ src/sections/Workflow.jsx
@@ -1,5 +1,31 @@
+import { workflow } from '../constants';
+
 const Workflow = () => {
-  return <div>Workflow</div>;
+  return (
+    <section className="mt-6">
+      <h1 className="font-bold text-darkGrayBlue text-2xl">
+        Supercharge your workflow
+      </h1>
+      <p className="text-grayBlue mt-2 mb-6">
+        We've got the tools to boost your productivity.
+      </p>
+
+      <div className="mb-12 lg:flex lg:items-center gap-12 lg:gap-36">
+        {workflow.map((item, index) => (
+          <div
+            key={index}
+            className="p-4 flex flex-col items-center justify-center"
+          >
+            <img src={item.icon} alt="icon" width={30} />
+            <h2 className="font-bold mt-2 mb-4 text-xl text-darkGrayBlue">
+              {item.heading}
+            </h2>
+            <p className="sm:max-w-md lg:max-w-lg">{item.text}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Workflow;
