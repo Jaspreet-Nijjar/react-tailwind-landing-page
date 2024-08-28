c13470c6811c6a89f15e0e75bc8a3ebcd8d4e965
Jaspreet <jaspreetkn92@gmail.com>
2024-08-28 15:29:32 +0100

    created footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,33 @@
+import logo from '../../images/logo.svg';
+import { footerLinks, socialMediaIcons } from '../constants';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-100 p-8 w-screen flex flex-col items-center lg:flex-row lg:justify-around">
+      <div className="flex items-center">
+        <a href="/">
+          <img src={logo} alt="clipboard logo" className="w-12" />
+        </a>
+      </div>
+
+      {footerLinks.map((link) => (
+        <div className="flex flex-col">
+          <a
+            className="mt-4 text-darkGrayBlue hover:text-cyan transition-all"
+            href={link.link}
+          >
+            {link.linkName}
+          </a>
+        </div>
+      ))}
+
+      <div className="flex gap-12 justify-between mt-12 mb-6">
+        {socialMediaIcons.map((icon, index) => (
+          <img src={icon.image} key={index} />
+        ))}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
